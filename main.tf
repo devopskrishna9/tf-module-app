ff9759e124c3e409bd706ab2852fb3b88ddc0813
krishna <brkkrishna9@gmail.com>
2023-02-27 00:05:55 +0530

    new changes

--- main.tf
+++ main.tf
@@ -24,33 +24,4 @@ resource "aws_security_group" "main" {
   )
 }
 
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "${var.env}-docdb-cluster"
-  engine                  = "docdb"
-  engine_version = var.engine_version
-  master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
-  master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
-  skip_final_snapshot     = true
-  db_subnet_group_name = aws_docdb_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.docdb.id]
-  storage_encrypted = true
-  kms_key_id = data.aws_kms_key.key.arn
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-cluster" }
-  )
-}
-
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = var.number_of_instances
-  identifier         = "${var.env}-docdb-cluster-instance-${count.index}"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = var.instance_class
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-cluster-instance-${count.index}" }
-  )
-}
 
