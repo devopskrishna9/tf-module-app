2d75d3014016c7162f06101d3321b1674bc57671
krishna <brkkrishna9@gmail.com>
2023-02-27 17:17:09 +0530

    new changes

--- main.tf
+++ main.tf
@@ -24,4 +24,28 @@ resource "aws_security_group" "main" {
   )
 }
 
+resource "aws_launch_template" "main" {
+  name_prefix = "${var.env}-${var.component}-template"
+  image_id = data.aws_ami.centos8.id
+  instance_type = var.instance_type
+}
 
+resource "aws_autoscaling_group" "asg" {
+  name                      = "${var.env}-${var.component}-asg"
+  max_size                  = var.max_size
+  min_size                  = var.min_size
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = var.desired_capacity
+  force_delete              = true
+  vpc_zone_identifier       = var.subnet_ids
+
+  dynamic "tag" {
+    for_each = var.all_tags
+    content {
+      key = each.key
+      value = each.value
+      propagate_at_launch = true
+    }
+  }
+}
