a8bc8b18609c194d7884a9f3132e60c1dc19c0eb
krishna <brkkrishna9@gmail.com>
2023-02-27 23:32:07 +0530

    new changes

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/devopskrishna9/roboshop-ansible roboshop.yml -e ROLE_NAME=${component} -e env=${env}
+ansible-pull -i localhost, -U https://github.com/devopskrishna9/roboshop-ansible roboshop.yml -e ROLE_NAME=${component} -e env=${env}  | tee /opt/ansible.log
