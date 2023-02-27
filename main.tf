3f0cd7c4e4831a93cf7d31f4f2a589c1c37aedee
krishna <brkkrishna9@gmail.com>
2023-02-27 20:39:12 +0530

    new changes

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_iam_policy" "policy" {
           "ssm:GetParameter"
         ],
         "Resource" : [ "arn:aws:ssm:us-east-1:718756811784:parameter/${var.env}.${var.component}*",
-                       "arn:aws:ssm:us-east-1:718756811784:parameter/nexus"
+                       "arn:aws:ssm:us-east-1:718756811784:parameter/nexus*"
         ]
       },
       {
