00fc8f3ef46aa52a9ed38398d8812743d751071d
krishna <brkkrishna9@gmail.com>
2023-03-15 18:21:06 +0530

    new changes made

--- main.tf
+++ main.tf
@@ -97,19 +97,17 @@ resource "aws_security_group" "main" {
   )
 }
 
-
-
 resource "aws_launch_template" "main" {
   name = "${var.env}-${var.component}-template"
   image_id = data.aws_ami.centos8.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
-#  //user_data  = base64encode(templatefile("${path.module}/user-data.sh", { component = var.component, env = var.env }))
-#
+  user_data  = base64encode(templatefile("${path.module}/user-data.sh", { component = var.component, env = var.env }))
+
   iam_instance_profile {
     arn = aws_iam_instance_profile.profile.arn
   }
-#
+
 #  instance_market_options {
 #    market_type = "spot"
 #  }
